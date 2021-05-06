25f5220b180c412f1cb93fa22e311a8998ec5621
Asvin Goel <goel@telematique.eu>
2021-05-07 00:13:01 +0200

    CORS

--- server.js
+++ server.js
@@ -10,14 +10,7 @@ const app = express();
 
 const server = http.createServer(app);
 //const io = socketio(server);
-const io = socketio(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
-});
+const io = socketio(server);
 
 // Set root for URL
 app.use(express.static(path.join(__dirname, '/www/')));
